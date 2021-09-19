05e917810ab79c1ad1e2ae3346708c42fef1d80d
Yazan <yazana@ilsny.com>
2021-09-19 19:07:57 -0400

    added event-bus and query service

---
+++ event-bus/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const axios = require("axios");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => console.log("listening on port 4005"));
